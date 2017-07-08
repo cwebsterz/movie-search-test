827092b07728ad103ecab7c38cc5057a8339cc66
cwebsterz <cwebsterzornes@live.com>
2017-07-08 11:45:44 -0400

    completed step 5

--- src/store.js
+++ src/store.js
@@ -4,6 +4,7 @@ import app from './reducers/app'
 import favorites from './reducers/favorites'
 import favorite from './reducers/favorite'
 import search from './reducers/search'
+import { SET_FAVORITES } from './constants'
 
 const store = createStore(
 	combineReducers({
@@ -14,5 +15,15 @@ const store = createStore(
 	}),
 	applyMiddleware(thunk)
 )
+
+function listAlbums(dispatch, getState) {
+	const url = process.env.REACT_APP_API
+
+	fetch(url + '/favorites').then(res => res.json()).then(favorites => {
+		dispatch({ type: SET_FAVORITES, payload: favorites })
+	})
+}
+
+store.dispatch(listAlbums)
 
 export default store
